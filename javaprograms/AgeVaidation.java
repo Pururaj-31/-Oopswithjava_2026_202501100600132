ef00bc00fab7d402a9433deb029ca26cf2073b02
Pururaj-31 <satsangipururaj@gmail.com>
2026-09-12 11:49:53 +0530

    Add files via upload

---
+++ javaprograms/AgeVaidation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class AgeVaidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        try {
+            checkAge(a);
+        } catch (AgeInvalidException e) {
+            System.out.println(e);
+        } finally{
+            sc.close();
+        }
+    }
+    static void checkAge(int age) throws  AgeInvalidException {
+        if(age<18) throw new AgeInvalidException("Age is not valid to vote");
+        System.out.println("Eligible is vote");
+    }
+}
+class AgeInvalidException extends Exception{
+    AgeInvalidException(String msg) {
+        super(msg);
+    }
+}
